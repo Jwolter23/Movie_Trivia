39b0d4c8bd78f1e2f98104f4aa656977d75cccc1
Jordan Wolter <jwolter456@gmail.com>
2022-10-15 21:26:42 -0400

    updated what I used for math.radom and have question looping functioning

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ let randomQuestion
 let currentQuestion
 
 //function will begin the game when the start button is clicked on and begin to roll out questions from our questionUp function
+// used part of stack overflow to see how i can use .sort and random and how -.5 will give it a 50 50 chance
+//https://stackoverflow.com/questions/53591691/sorting-an-array-in-random-order
 startButton.addEventListener('click', startGame)
 
 function startGame() {
@@ -23,7 +25,7 @@ function questionsUp() {
 }
 
 function beginQuestions(question) {
-    questionContainer.innerText = question.questions
+    questionContainer.innerText = question.question
 }
 
 function answerCheck() {
@@ -44,12 +46,26 @@ const questions = [
         b: '1997', typeq: false,
         c: '1999', typeq: false,
         d: '2000', typeq: false
-    }}
+    }},
     {question: 'Who said the following: Unfortunately no one can be told what the Matrix is, you have to see it for yourself.',
     answer: {
         a: 'Trinity', typeq: false,
         b: 'Cypher', typeq: false,
         c: 'Morpheus', typeq: true,
         d: 'Agent Smith', typeq: false
+    }},
+    {question: 'what type of animal does a sentinel look like?',
+    answer: {
+        a: 'Dolphin', typeq: false,
+        b: 'Squid', typeq: true,
+        c:  'Bird', typeq: false,
+        d:  'Snake', typeq: false
+    }},
+    {question: 'what is Neos real name in the movie?',
+    answer: {
+        a: 'Thomas Anderson', typeq: true,
+        b:  'Laurence Fishburne', typeq: false,
+        c: 'Adam Smith', typeq: false,
+        d: 'Hugo Weaving', typeq: false
     }}
 ]
