efad6c638b84c977ace17536d48e2b668d889ef4
Jordan Wolter <jwolter456@gmail.com>
2022-10-15 20:52:42 -0400

    Random question function set up but not tested

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 let startButton = document.querySelector('.startButton')
 
+let questionContainer = document.querySelector('.containerHead')
+let answers = document.querySelector('answerButtons')
 
+//equal to undefined to begin with will use these two to randomize questions and keep track of what questions we have
+let randomQuestion
+let currentQuestion
 
 //function will begin the game when the start button is clicked on and begin to roll out questions from our questionUp function
 startButton.addEventListener('click', startGame)
 
 function startGame() {
-    console.log('started')
+    console.log('working')
+    randomQuestion = questions.sort(() => Math.random() - .5)
+    currentQuestion = 0
     questionsUp()
 }
 
 function questionsUp() {
+    beginQuestions(randomQuestion[currentQuestion])
 
+}
+
+function beginQuestions(question) {
+    
 }
 
 function answerCheck() {
@@ -28,9 +40,9 @@ function answerCheck() {
 const questions = [
     {question: 'What year was the matrix released?',
     answers: {
-        a: '1998', type: true,
-        b: '1997', type: false,
-        c: '1999', type: false,
-        d: '2000', type: false
+        a: '1998', typeq: true,
+        b: '1997', typeq: false,
+        c: '1999', typeq: false,
+        d: '2000', typeq: false
     }}
 ]
